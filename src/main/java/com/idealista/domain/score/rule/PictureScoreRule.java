8f7b21096e46d8e162034a448971c0e7949a702b
Juan Enrique Aicardo García <salkerps3@gmail.com>
2021-02-11 15:02:17 +0100

    Test for PictureScoreRule

--- src/main/java/com/idealista/domain/score/rule/PictureScoreRule.java
+++ src/main/java/com/idealista/domain/score/rule/PictureScoreRule.java
@@ -12,7 +12,6 @@ public class PictureScoreRule implements ScoreRule {
     private static final Integer BASE_RULE_SCORE = 0;
     private static final Integer PICTURE_SD_SCORE = 10;
     private static final Integer PICTURE_HD_SCORE = 20;
-    private static final Integer PICTURE_DIFFERENT_EXT_SCORE = 0;
     private static final Integer NO_PICTURE_SCORE = -10;
 
     @Autowired
@@ -26,11 +25,8 @@ public class PictureScoreRule implements ScoreRule {
                             pic -> {
                                 Integer score = ad.getScore() == null ? BASE_RULE_SCORE : ad.getScore();
                                 PictureVO picture = pictureListRepository.getPictureById(pic);
-                                if (picture.getQuality().equals("SD"))
-                                    ad.setScore(PICTURE_SD_SCORE + score);
-                                else if (picture.getQuality().equals("HD"))
-                                    ad.setScore(PICTURE_HD_SCORE + score);
-                                else ad.setScore(PICTURE_DIFFERENT_EXT_SCORE + score);
+                                if (picture.getQuality().equals("HD")) ad.setScore(PICTURE_HD_SCORE + score);
+                                else ad.setScore(PICTURE_SD_SCORE + score);
                             });
         } else {
             ad.setScore(pictureScore +  NO_PICTURE_SCORE);
---
+++ src/test/java/com/idealista/domain/score/PictureScoreRuleTest.java
@@ -0,0 +1,52 @@
+package com.idealista.domain.score;
+
+import com.idealista.TestUtil;
+import com.idealista.domain.model.entity.AdVO;
+import com.idealista.domain.score.rule.PictureScoreRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PictureScoreRuleTest {
+
+    @Autowired
+    private PictureScoreRule pictureScoreRule;
+
+    @Test
+    public void whenADVoHasNoPicture_shouldReturnMinus10ToScore() throws Exception {
+        AdVO ad = TestUtil.fakeAdVO(1, "", "", Collections.emptyList(), null, null, null, null);
+        pictureScoreRule.execute(ad);
+        assertEquals(-10, (int) ad.getScore());
+    }
+
+    @Test
+    public void whenADVoHasOneHDPicture_shouldReturnMinus20ToScore() throws Exception {
+        AdVO ad = TestUtil.fakeAdVO(1, "", "", List.of(2), null, null, null, null);
+        pictureScoreRule.execute(ad);
+        assertEquals(20, (int) ad.getScore());
+    }
+
+    @Test
+    public void whenADVoHasOnePictureNotHD_shouldReturnMinus20ToScore() throws Exception {
+        AdVO ad = TestUtil.fakeAdVO(1, "", "", List.of(1), null, null, null, null);
+        pictureScoreRule.execute(ad);
+        assertEquals(10, (int) ad.getScore());
+    }
+
+    @Test
+    public void whenADVoHasMoreThanOnePicture_shouldReturnMinus20ToScore() throws Exception {
+        AdVO ad = TestUtil.fakeAdVO(1, "", "", List.of(1,2), null, null, null, null);
+        pictureScoreRule.execute(ad);
+        assertEquals(30, (int) ad.getScore());
+    }
+
+}
